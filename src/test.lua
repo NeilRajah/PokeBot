a1cab15076f4b3463f17f289ba4fe05f1fda9177
NeilRajah <neil.balaskandarajah@gmail.com>
2019-11-06 12:17:50 -0500

    Need to dive DEEP into memory

--- src/test.lua
+++ src/test.lua
@@ -14,7 +14,7 @@ function main()
 
     --detect button press
     j = joypad.get(1)
-    if j.A then
+    if j.B then
         gui.text(2,20, "B Button Pressed")
     end
 
@@ -30,46 +30,59 @@ function main()
     --     print("been 30 frames")
     --     counter = 0
     -- end
+    gui.text(20,140, string.format(memory.readwordunsigned(0x2886)))
 end
 
-gui.register(main) --register for graphics, input uses frameadvance
+gui.register(main) --register for graphics, input uses frameadvance51
 
 -- move for a number of tiles
-function move(val, n) 
-
+function move(val, n, run) 
+    setAllFalse()
+    print("move ", val, n, run)
     if val == 'd' then --set everything else to false, only one can be true
-        print("pressButton ", val, n)
         j.down = true
-        j.right = false
-        j.left = false
-        j.up = false
     elseif val == 'r' then
-        print("pressButton ", val, n)
-        j.down = false
         j.right = true
-        j.left = false
-        j.up = false
     elseif val == 'l' then
-        print("pressButton ", val, n)
-        j.down = false
-        j.right = false
         j.left = true
-        j.up = false
     elseif val == 'u' then 
-        print("pressButton ", val, n)
-        j.down = false
-        j.right = false
-        j.left = false
         j.up = true
     end
 
-    for i = 0, 15 * n, 1 do --hold input for 15 frames
+    if run then
+        j.B = true
+    end
+
+    for i = 1, 7 * n, 1 do --hold input for n frames
         joypad.set(1, j)
         emu.frameadvance()
     end
 end
 
-move('r', 3)
-move('u', 4)
-move('r', 1)
-move('d', 1)
+function setAllFalse()
+    for key, value in pairs(j) do
+        j[key] = false
+    end
+end    
+
+function delay(num)
+    print("delaying ", num)
+    for i = 0, num, 1 do
+        emu.frameadvance()
+    end
+end
+
+steps = 2
+delay(40)
+-- for i = 1, 10, 1 do
+--     move('r', steps, true)
+--     delay(50)
+--     move('l', steps, true)
+--     delay(50)
+-- end
+move('r', 1, false)
+delay(40)
+
+--if facing in same direction, 5 frames
+--if facing perpendicular, 8 frames
+--if facing opposite, 7 frames
